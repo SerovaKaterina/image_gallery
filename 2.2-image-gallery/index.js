0e7b9764d00c1e147ff38f1af726f785a1c441f2
SerovaKaterina <k.serova.k@yandex.ru>
2024-09-29 23:38:45 +0300

    fix:add changes

--- 2.2-image-gallery/index.js
+++ 2.2-image-gallery/index.js
@@ -2,6 +2,10 @@ const accessKey = '9g2T6mQON_wPHwNtCu2di_oo_-e9J3FXvo5TMrR5DHU';
 const photoContainer = document.getElementById('photo-container');
 const searchInput = document.getElementById('search-input');
 const clearSearch = document.getElementById('clear-search');
+const searchIcon = document.getElementById('search-icon');
+const modal = document.getElementById('modal');
+const modalImg = document.getElementById('modal-img');
+const closeModal = document.getElementById('close-modal');
 
 async function fetchPhotos(query = '') {
     let url = `https://api.unsplash.com/photos?client_id=${accessKey}&per_page=12`;
@@ -34,6 +38,10 @@ searchInput.addEventListener('keydown', (event) => {
     }
 });
 
+searchIcon.addEventListener('click', () => {
+    fetchPhotos(searchInput.value);
+});
+
 clearSearch.addEventListener('click', () => {
     searchInput.value = '';
     searchInput.placeholder = 'Search for photos...';
@@ -45,3 +53,20 @@ window.onload = () => {
     searchInput.focus();
     searchInput.autocomplete = 'off';
 };
+
+photoContainer.addEventListener('click', (event) => {
+    if (event.target.tagName === 'IMG') {
+        modal.style.display = 'flex';
+        modalImg.src = event.target.src;
+    }
+});
+
+closeModal.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+});
